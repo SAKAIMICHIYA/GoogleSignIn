0cd1b6416da45d635a1049b75c90044e815912fb
m-sakai <re0026ir@gmail.com>
2018-07-03 23:24:19 +0900

    GoogleSignIn ver0.1 設定中

---
+++ GoogleSignIn-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  GoogleSignIn-Bridging-Header.h
+//  GoogleSignIn
+//
+//  Created by 酒井理也 on 2018/07/03.
+//  Copyright © 2018年 酒井理也. All rights reserved.
+//
+
+#ifndef GoogleSignIn_Bridging_Header_h
+#define GoogleSignIn_Bridging_Header_h
+
+
+#endif /* GoogleSignIn_Bridging_Header_h */
+
+#import <Google/SignIn.h>
